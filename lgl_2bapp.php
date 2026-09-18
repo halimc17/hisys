0df5af6187524dad6ac785693a227427d0207ecd
sendibagaskara <sendibagaskara6@gmail.com>
2026-09-18 14:03:31 +0700

    Selesaikan Laporan BAPP (BA Pelaksanaan Pekerjaan)
    - Lengkapi lgl_slave_2bapp.php yang sebelumnya masih stub kosong: sekarang
      menampilkan 1 baris per realisasi (blok/kegiatan/termin) dari log_baspk,
      digabung dengan data kontrak (lgl_pengajuanspk_keg) berdampingan - satuan,
      HK, dan jumlah rupiah versi kontrak vs realisasi.
    - Nama Blok mengikuti jenis SPK: PROJECT ambil dari tabel project, jenis lain
      pakai nama organisasi (konsisten dengan perbaikan kolom Divisi di Daftar SPK).
    - Tambah subtotal per No Transaksi, selain Grand Total di baris terakhir.
    - Tambah filter Jenis SPK di form (lgl_2bapp.php), dan aktifkan kembali
      filter Kontraktor & No Transaksi yang sebelumnya disembunyikan.
    - Ubah layout form filter dari satu baris horizontal jadi tersusun ke bawah,
      konsisten dengan Laporan Daftar SPK.

--- lgl_2bapp.php
+++ lgl_2bapp.php
@@ -31,39 +31,57 @@
 	foreach ($res as $bar) {
 		$optsupp.="<option value='".$bar['koderekanan']."'>".getNamaSupplier($bar['koderekanan'])."</option>";
 	}
-	
-	echo"<fieldset style=float:left><legend><b>Filter </b></legend>
+
+	$optjenis="<option value='%%'>".$_SESSION['lang']['all']."</option>";
+	$arrjenis=getEnum($dbname,'lgl_pengajuanspkht','jenis');
+	foreach ($arrjenis as $val) {
+		$optjenis.="<option value='".$val."'>".$val."</option>";
+	}
+
+	echo"<fieldset style=width:300px;float:left><legend><b>Filter</b></legend>
 	<table>
-		<tr>	
+		<tr>
 			<td>".$_SESSION['lang']['pt']."</td>
 			<td>:</td>
 			<td><select id=pt class=select2 style=\"width:180px;\" onchange=\"getunit(this);\">".$optpt."</select></td>
-
+		</tr>
+		<tr>
 			<td>".$_SESSION['lang']['unit']."</td>
 			<td>:</td>
 			<td><select id=unit class=select2 style=\"width:180px;\" >".$optunit."</select></td>
-
-			<td hidden>".$_SESSION['lang']['kontraktor']."</td>
-			<td hidden>:</td>
-			<td hidden><select id=kontraktor class=select2 style=\"width:180px;\" >".$optsupp."</select></td>
-
-			<td hidden>".$_SESSION['lang']['notransaksi']."</td>
-			<td hidden>:</td>
-			<td hidden><input id=notransaksi onkeypress='enterkey(event,loaddata)' class=myinputtext onkeydown=\"upperCaseF(this)\" style=\"width:178px;\"></td>
-				
+		</tr>
+		<tr>
+			<td>".$_SESSION['lang']['kontraktor']."</td>
+			<td>:</td>
+			<td><select id=kontraktor class=select2 style=\"width:180px;\" >".$optsupp."</select></td>
+		</tr>
+		<tr>
+			<td>".$_SESSION['lang']['jenis']."</td>
+			<td>:</td>
+			<td><select id=jenis class=select2 style=\"width:180px;\" >".$optjenis."</select></td>
+		</tr>
+		<tr>
+			<td>".$_SESSION['lang']['notransaksi']."</td>
+			<td>:</td>
+			<td><input id=notransaksi onkeypress='enterkey(event,loaddata)' class=myinputtext onkeydown=\"upperCaseF(this)\" style=\"width:178px;\"></td>
+		</tr>
+		<tr>
 			<td>".$_SESSION['lang']['tanggal']."</td>
 			<td>:</td>
 			<td>
 				<input style=\"width:75px;text-align:center\" type='text' placeholder='Klik Tanggal' class='myinputtext' id='tgl1' readonly onmousemove='setCalendar(this.id)' onkeypress='return false;'  size='10' maxlength='10'/>
 				s/d  <input style=\"width:75px;text-align:center\" type='text' placeholder='Klik Tanggal' class='myinputtext' id='tgl2' readonly onmousemove='setCalendar(this.id)' onkeypress='return false;'  size='10' maxlength='10'/>
 			</td>
-
+		</tr>
+		<tr>
 			<td colspan=2 align=left></td>
 			<td  align=left colspan=20>
 			<button onclick=preview('html') class=mybutton name=preview id=preview>".$_SESSION['lang']['preview']."</button>
 			<button onclick=preview('excel') class=mybutton name=preview id=excel>".$_SESSION['lang']['excel']."</button>
 			<button onclick=preview('pdf') class=mybutton name=preview id=pdf>".$_SESSION['lang']['pdf']."</button>
 			</td>
+		</tr>
+		<tr>
 			<td colspan=2></td>
 			<td align=center>
 				<button onclick=batal() class=mybutton name=btnBatal id=btnBatal style='width:100%'>".$_SESSION['lang']['cancel']."</button>
